3b846767426b539b2b55f4036f3830f4298c6da7
hai <lijie-15@126.com>
2013-10-08 19:30:13 +0800

    Signed-off-by: hai <lijie-15@126.com>

---
+++ gen/com/zhipinnet/xs/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zhipinnet.xs;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/zhipinnet/xs/MainActivity.java
@@ -0,0 +1,58 @@
+package com.zhipinnet.xs;
+
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+    	System.setProperty("http.keepAlive", "false");
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html", 3000);
+        //if(checkNetWorkStatus()){
+    	//}
+    }
+    
+    /**
+	 * check network Status
+	 * 
+	 * @return boolean
+	 */
+
+	public boolean checkNetWorkStatus() {
+		boolean result;
+		ConnectivityManager cm = (ConnectivityManager) this
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netinfo = cm.getActiveNetworkInfo();
+		if (netinfo != null && netinfo.isConnected()) { // The current network is not available
+			result = true;
+		} else { //  not available
+			new AlertDialog.Builder(MainActivity.this).setMessage(
+					"The available network connection, please open the network connections").setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialoginterface,
+								int i) {
+							ComponentName cn = new ComponentName(
+									"com.android.settings",
+									"com.android.settings.Settings");
+							Intent intent = new Intent();
+							intent.setComponent(cn);
+							intent.setAction("android.intent.action.VIEW");
+							startActivity(intent);
+							finish();
+						}
+					}).show();
+			result = false;
+		}
+		return result;
+	}
+
+}
